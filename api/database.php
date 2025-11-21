fa0d161d560774eafea801a41e4bd819ac610a6d
miguelzackk <mzsilvaw@gmail.com>
2025-11-21 14:31:13 -0300

    feat: finish desktop version.

--- api/database.php
+++ api/database.php
@@ -20,8 +20,6 @@ $password = 'root';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo json_encode(['error' => 'Erro na conexão: ' . $e->getMessage()]);
-    exit();
+} catch (PDOException $e) {
+    die("Erro ao conectar: " . $e->getMessage());
 }
-?>
